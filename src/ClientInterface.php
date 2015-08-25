8c7a5f52401005f5fe8e4331c99b551c8d424cb5
cdekok <c.dekok@picturae.nl>
2015-08-25 14:15:21 +0200

    Update interface

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -5,9 +5,56 @@ interface ClientInterface
 {
     /**
      * Get deed by id
-     * 
+     *
      * @param string $uuid
      * @return \stdClass|null
      */
     public function getDeed($uuid);
+
+    /**
+     * Get deeds result set
+     *
+     * @param array $query
+     * @return \stdClass
+     */
+    public function getDeeds($query);
+
+    /**
+     * Get deed by id
+     *
+     * @param string $uuid
+     * @return \stdClass|null
+     */
+    public function getPerson($uuid);
+
+    /**
+     * Get persons result set
+     *
+     * @param array $query
+     * @return \stdClass
+     */
+    public function getPersons($query);
+
+    /**
+     * Get deed by id
+     *
+     * @param string $uuid
+     * @return \stdClass|null
+     */
+    public function getRegister($uuid);
+
+    /**
+     * Get deed result set
+     *
+     * @param array $query
+     * @return \stdClass
+     */
+    public function getRegisters($query);
+
+    /**
+     * Get api key
+     *
+     * @return string
+     */
+    public function getApiKey();
 }
