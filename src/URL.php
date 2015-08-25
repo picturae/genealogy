279d9cc1bc6e75cf5494c67880c78f59a9733242
cdekok <c.dekok@picturae.nl>
2015-08-25 16:54:16 +0200

    Add PHPMD

--- src/URL.php
+++ src/URL.php
@@ -92,14 +92,15 @@ class URL
      */
     private function getCurrentScheme()
     {
+        $protocol = 'http://';
+        
         if (isset($_SERVER['HTTPS']) &&
             ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
             isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
             $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
             $protocol = 'https://';
-        } else {
-            $protocol = 'http://';
         }
+        
         return $protocol;
     }
 }
