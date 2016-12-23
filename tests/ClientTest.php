a5fc27b840c8d38fd4ccaaff20f0ac67d6ca34b4
cdekok <chris.de.kok@gmail.com>
2016-12-23 11:54:41 +0100

    Update guzzle / phpunit

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,49 +1,50 @@
 <?php
 namespace Test\Picturae\Genealogy;
 
-class ClientTest extends \PHPUnit_Framework_TestCase
+class ClientTest extends \PHPUnit\Framework\TestCase
 {
     private $key = '509544d0-1c67-11e4-9016-c788dee409dc';
-    
+
     public function testConstruct()
     {
         new \Picturae\Genealogy\Client($this->key);
     }
-    
+
     public function testGetApiKey()
     {
         $client = new \Picturae\Genealogy\Client($this->key);
         $this->assertEquals($this->key, $client->getApiKey());
     }
-    
+
     public function testGetDeed()
     {
-        $json = file_get_contents(__DIR__ . '/result/deed.json');
-        $body = \GuzzleHttp\Stream\Stream::factory($json);
-        $mock = new \GuzzleHttp\Subscriber\Mock([
-            new \GuzzleHttp\Message\Response(200, [], $body)
+        $response = new \GuzzleHttp\Psr7\Response('200', [], __DIR__ . '/result/deed.json');
+        $handler = new \GuzzleHttp\Handler\MockHandler([
+            $response
         ]);
         
-        $client = new \Picturae\Genealogy\Client($this->key);
-        $client->getClient()->getEmitter()->attach($mock);
-        
+        $client = new \Picturae\Genealogy\Client($this->key, [
+            'handle' => $handler
+        ]);
+
         $id = '01792869-7556-f1ad-ef04-aacb0be11b49';
         $deed = $client->getDeed($id);
         $this->assertEquals($deed->id, $id);
     }
-    
+
     public function testGetDeeds()
     {
-        $json = file_get_contents(__DIR__ . '/result/deed-list.json');
-        $body = \GuzzleHttp\Stream\Stream::factory($json);
-        $mock = new \GuzzleHttp\Subscriber\Mock([
-            new \GuzzleHttp\Message\Response(200, [], $body)
+        //$json = file_get_contents(__DIR__ . '/result/deed-list.json');
+        $response = new \GuzzleHttp\Psr7\Response('200', [], __DIR__ . '/result/deed-list.json');
+        $handler = new \GuzzleHttp\Handler\MockHandler([
+            $response
         ]);
         
-        $client = new \Picturae\Genealogy\Client($this->key);
-        $client->getClient()->getEmitter()->attach($mock);
-        
+        $client = new \Picturae\Genealogy\Client($this->key, [
+            'handle' => $handler
+        ]);
+
         $deeds = $client->getDeeds();
-        $this->assertEquals(count($deeds->deed), 1);
+        $this->assertEquals(count($deeds->deed), 100);        
     }
 }
